285bdf1f7a0f336fbf03818a1205e1daf397f7b3
Tran Van Tam <tampfievk50@gmail.com>
2025-11-20 13:14:58 +0700

    update code

--- Main.go
+++ Main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/zeromicro/go-zero/rest"
 )
 
-var configFile = flag.String("f", "etc/Main.yaml", "the config file")
+var configFile = flag.String("f", "application/etc/Main.yaml", "the config file")
 
 func main() {
 	flag.Parse()
